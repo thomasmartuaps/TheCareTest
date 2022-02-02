37ec5fe7111eac115a64b7039bdba2a35dada929
Thomas <thomasmartuaps@gmail.com>
2022-02-02 21:31:44 +0700

    image upload to cloudinary

--- MobileBackend/src/containers/ProductForm.tsx
+++ MobileBackend/src/containers/ProductForm.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useState } from "react";
 import { Text, View, TextInput, Pressable } from "react-native";
+import ImagePicker from "react-native-image-picker";
 
 import styles from "../styles";
 import { CATEGORIES } from "../constants";
@@ -13,11 +14,52 @@ export default function NavigationPage() {
   const [stock, setStock] = useState<Number>(0);
   const [price, setPrice] = useState<Number>(0);
   const [image, setImage] = useState<String>("");
+
+  const selectFile = () => {
+    var options = {
+      title: "Select Image",
+
+      customButtons: [
+        {
+          name: "customOptionKey",
+
+          title: "Choose file from Custom Option",
+        },
+      ],
+
+      storageOptions: {
+        skipBackup: true,
+
+        path: "images",
+      },
+    };
+
+    ImagePicker.showImagePicker(options, (res) => {
+      console.log("Response = ", res);
+
+      if (res.didCancel) {
+        console.log("User cancelled image picker");
+      } else if (res.error) {
+        console.log("ImagePicker Error: ", res.error);
+      } else if (res.customButton) {
+        console.log("User tapped custom button: ", res.customButton);
+
+        alert(res.customButton);
+      } else {
+        let source = res;
+
+        this.setState({
+          resourcePath: source,
+        });
+      }
+    });
+  };
+
   return (
     <View style={styles.background}>
       <View style={styles.form}>
         <View style={{ width: 333, marginTop: 44, marginBottom: 22 }}>
-          <Text style={{ fontWeight: "700", fontSize: 37 }}>
+          <Text style={{ fontWeight: "700", fontSize: 37, width: 333 }}>
             Add New Product
           </Text>
         </View>
@@ -46,13 +88,18 @@ export default function NavigationPage() {
         </View>
         <View style={{ width: 333, marginTop: 10, marginBottom: 10 }}>
           <TextInput
-            style={styles.textInput}
+            style={[styles.textInput, { width: 116 }]}
             keyboardType="number-pad"
             onChangeText={(stock) => setStock(Number(stock))}
             placeholder="Stock"
           ></TextInput>
         </View>
-        <View style={{ width: 333, marginTop: 10, marginBottom: 10 }}>
+        <View
+          style={[
+            styles.categorySection,
+            { width: 375, marginTop: 10, marginBottom: 10 },
+          ]}
+        >
           {CATEGORIES.map((category) => {
             const isSelected = tags[0] === category ? true : false;
             return (
@@ -67,19 +114,37 @@ export default function NavigationPage() {
                   setTags(newTags);
                 }}
               >
-                <Text>{category}</Text>
+                <Text
+                  style={
+                    isSelected
+                      ? styles.selectedCategoryText
+                      : styles.categoryText
+                  }
+                >
+                  {category}
+                </Text>
               </Pressable>
             );
           })}
         </View>
         <View style={{ width: 333, marginTop: 10, marginBottom: 10 }}>
           <TextInput
-            style={styles.textInput}
+            style={[styles.textInput, { width: 155 }]}
             keyboardType="number-pad"
             onChangeText={(price) => setPrice(Number(price))}
             placeholder="Price"
           ></TextInput>
         </View>
+      </View>
+      <View style={{ width: 333, marginTop: 10, marginBottom: 10 }}>
+        <Pressable style={styles.publishButton}>
+          <Text style={styles.publishText}>'Upload Image'</Text>
+        </Pressable>
+      </View>
+      <View style={{ width: 333, marginTop: 10, marginBottom: 10 }}>
+        <Pressable style={styles.publishButton}>
+          <Text style={styles.publishText}>'Publish'</Text>
+        </Pressable>
       </View>
     </View>
   );
--- MobileBackend/src/styles.ts
+++ MobileBackend/src/styles.ts
@@ -2,13 +2,12 @@ import { StyleSheet, Platform } from "react-native";
 
 const baseStyles = StyleSheet.create({
   background: {
-    flex: 1,
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center",
+    // justifyContent: "center",
     backgroundColor: "#E7F5FD",
     width: "100%",
-    height: "100%",
+    // height: 3000,
     // padding: 24
   },
   form: {},
@@ -23,14 +22,77 @@ const baseStyles = StyleSheet.create({
     fontSize: 18,
     minHeight: 55,
     paddingBottom: 8,
-    fontFamily: Platform.OS === "android" || "web" ? "Roboto" : "Arial",
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Arial",
   },
   categoryButton: {
+    // flex: 2,
+    flexWrap: "wrap",
+    alignContent: "center",
+    justifyContent: "center",
     color: "#3549FB",
     backgroundColor: "#CAECFF",
     borderRadius: 9,
+    minWidth: 113,
+    maxWidth: 155,
+    height: 49,
+    margin: 10,
+  },
+  inputTitle: {
+    color: "#3549FB",
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Arial",
     fontWeight: "500",
-    fontSize: 16.2587,
+    fontSize: 18,
+  },
+  categoryText: {
+    color: "#3549FB",
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Arial",
+    fontWeight: "500",
+    fontSize: 18,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  categorySection: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  selectedCategoryButton: {
+    // flex: 2,
+    flexWrap: "wrap",
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: "#2D9CDB",
+    color: "#FFF",
+    borderRadius: 9,
+    minWidth: 113,
+    maxWidth: 155,
+    height: 49,
+    margin: 10,
+  },
+  selectedCategoryText: {
+    color: "#FFF",
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Arial",
+    fontWeight: "500",
+    fontSize: 18,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  publishButton: {
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: "#56E4A0",
+    borderRadius: 9,
+    width: 333,
+    height: 60,
+    margin: 10,
+  },
+  publishText: {
+    color: "#FFF",
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Arial",
+    fontWeight: "700",
+    fontSize: 18,
+    marginLeft: 10,
+    marginRight: 10,
   },
 });
 
@@ -43,9 +105,10 @@ const advancedStyles = StyleSheet.create({
     backgroundColor: "#2D9CDB",
     color: "#FFF",
   },
+  selectedCategoryText: {
+    ...baseStyles.categorySection,
+    color: "#FFF",
+  },
 });
 
-export default {
-  ...baseStyles,
-  ...advancedStyles,
-};
+export default baseStyles;
